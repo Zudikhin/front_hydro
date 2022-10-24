dc093e10b4fe4fac9696a58ed03f376764bb339c
Zudikhin <uzudihin@gmail.com>
2022-10-24 18:07:13 +0300

    verstka

--- app/js/main.js
+++ app/js/main.js
@@ -1,58 +1,17 @@
 $(document).ready(function () {
     "use strict";
 
-    $(".nav_mobile_top_btn").click(function() {
-        $(this).toggleClass("active");
-        $(".nav_mobile_drop").toggleClass("active");
+    $(".filter-industries").click(function() {
+        $("#filter-industries").toggleClass("active");
     });
 
-    $(".blog_content_item_content_read_btn").click(function(){
-        $(this).parent().parent().parent().parent().addClass("active");
+    $(".filter-ps").click(function() {
+        $("#filter-ps").toggleClass("active");
     });
 
-    $('.game_one_slider').slick({
-        dots: false,
-        infinite: true,
-        arrows: false,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
+    $(".filter-projects").click(function() {
+        $("#filter-projects").toggleClass("active");
     });
 
-    $('.game_two_slider').slick({
-        dots: false,
-        infinite: true,
-        arrows: false,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-    });
-
-    $('.game_three_slider').slick({
-        dots: false,
-        infinite: true,
-        arrows: false,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-    });
-
-    $('.game_four_slider').slick({
-        dots: false,
-        infinite: true,
-        arrows: false,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-    });
-
-    $('.game_five_slider').slick({
-        dots: false,
-        infinite: true,
-        arrows: false,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-    });
 
 });
